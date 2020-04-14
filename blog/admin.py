88ff2eb0b0d2b9a2e736712f0ff049d6c2108107
Your Name <sanya.seredyak@gmail.com>
2020-04-14 09:58:16 +0300

    Add Blog app models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'created_at',)
+    inlines = (CommentInline,)
+
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Blog(models.Model):
+
+    class Meta:
+        verbose_name = "Обговорення"
+        verbose_name_plural = "Обговорення"
+        ordering = ("-created_at",)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    title = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+
+    class Meta:
+        verbose_name = "Коментар"
+        verbose_name_plural = "Коментарі"
+        ordering = ("-created_at",)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, blank=False, null=False)
+    body = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} to {self.blog.title}"
